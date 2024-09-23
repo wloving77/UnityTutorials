fc675df91c0d040ab9b00ec28b5609133d068f46
wloving77 <wloving77@gmail.com>
2024-09-22 20:59:24 -0400

    Finished second tutorial! Moving Sin Graph!
    Very cool!

--- The Basics/Graphs/Assets/Scripts/Graph.cs
+++ The Basics/Graphs/Assets/Scripts/Graph.cs
@@ -6,20 +6,32 @@ public class Graph: MonoBehaviour {
     [SerializeField] Transform pointPrefab;
     [SerializeField, Range(10,100)]int resolution = 10;
 
+    Transform[] points;
+
     void Awake() {
 
         float step = 2f / resolution;
         var position = Vector3.zero;
         var scale = Vector3.one * step;
-        for(int i = 0; i < resolution; i++) {
+        points = new Transform[resolution];
+        for(int i = 0; i < points.Length; i++) {
             Transform point = Instantiate(pointPrefab);
+            points[i] = point;
             position.x = (i + 0.5f) * step - 1f;
-            position.y = position.x * position.x;
             point.localPosition = position;  
             point.localScale = scale;
             point.SetParent(transform, false); 
         }
-    
+    }
+
+    void Update() {
+        float time = Time.time;
+        for(int i = 0; i < points.Length; i++) {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }
     }
 
 }
