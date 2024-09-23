5dbce9b2011200b137e55ee840e924db33e7b99e
wloving77 <wloving77@gmail.com>
2024-09-23 13:48:11 -0400

    Finished another tut, trigonometry is hard, gonna spend some time locking these concepts in.

--- The Basics/Graphs/Assets/Scripts/Graph.cs
+++ The Basics/Graphs/Assets/Scripts/Graph.cs
@@ -1,4 +1,3 @@
-using UnityEditor.Compilation;
 using UnityEngine;
 
 public class Graph: MonoBehaviour {
@@ -6,31 +5,35 @@ public class Graph: MonoBehaviour {
     [SerializeField] Transform pointPrefab;
     [SerializeField, Range(10,100)]int resolution = 10;
 
+    [SerializeField] FunctionLibrary.FunctionName function;
+
     Transform[] points;
 
     void Awake() {
-
         float step = 2f / resolution;
-        var position = Vector3.zero;
         var scale = Vector3.one * step;
-        points = new Transform[resolution];
+        points = new Transform[resolution * resolution];
         for(int i = 0; i < points.Length; i++) {
             Transform point = Instantiate(pointPrefab);
             points[i] = point;
-            position.x = (i + 0.5f) * step - 1f;
-            point.localPosition = position;  
             point.localScale = scale;
             point.SetParent(transform, false); 
         }
     }
 
     void Update() {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
-        for(int i = 0; i < points.Length; i++) {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
-            point.localPosition = position;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for(int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+            if (x==resolution){
+                x=0;
+                z++;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u,v,time);
         }
     }
 
